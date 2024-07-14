b3457013ca7cdc7d5a5777d9896eee2092821a1f
hi-rachel <rachel.uiux@gmail.com>
2024-07-14 22:56:08 +0900

    feat: implement sign-up and sign-in logic using appwrite

--- app/index.jsx
+++ app/index.jsx
@@ -5,8 +5,15 @@ import { ScrollView } from "react-native";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
 import { StatusBar } from "expo-status-bar";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if (!isLoading && isLoggedIn) {
+    return <Redirect href="/home" />;
+  }
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>

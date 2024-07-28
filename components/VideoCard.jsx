0d83bbf6913232e61721cd41c38740e493977831
hi-rachel <rachel.uiux@gmail.com>
2024-07-28 19:33:24 +0900

    feat: implement profile screen

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -10,7 +10,7 @@ const VideoCard = ({ video: { title, thumbnail, video, creator } }) => {
     <View className="flex flex-col items-center px-4 mb-14">
       <View className="flex flex-row gap-3 items-start">
         <View className="flex justify-center items-center flex-row flex-1">
-          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center">
             <Image
               source={{ uri: creator?.avatar }}
               className="w-full h-full rounded-lg"
